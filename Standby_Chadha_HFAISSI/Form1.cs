4e7b0d44b542479a08383c0ece68280199c749cd
pc <pc@DESKTOP-T2455K9>
2020-06-10 01:40:32 +0200

    Initial commit

---
+++ Standby_Chadha_HFAISSI/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Standby_Chadha_HFAISSI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            try
+            {
+                if (double.Parse(textBox3.Text) <= 1 && double.Parse(textBox3.Text) >= 0 && double.Parse(textBox2.Text) >= 0 && double.Parse(textBox2.Text) <= 1)
+                {
+                    int k = 0;
+                    double R = Standby.calcule(double.Parse(textBox1.Text), double.Parse(textBox2.Text), k);
+                    while (R < double.Parse(textBox3.Text))
+                    {
+                        k += 1;
+                        R += Standby.calcule(double.Parse(textBox1.Text), double.Parse(textBox2.Text), k);
+                    }
+                    MessageBox.Show("le nombre de composants est:" + k);
+                }
+                else MessageBox.Show("vérifier que R* et lamda appartient à [0;1]");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Standby_Chadha_HFAISSI/Standby.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Standby_Chadha_HFAISSI
+{
+    public class Standby
+    {
+        public static long Factoriel(int n )
+        {
+            return n > 1 ? n * Factoriel(n - 1) : 1;
+        }
+
+        public static double calcule (double t, double lamda , int k)
+        {
+            return (Math.Exp(-lamda * t) * Math.Pow(lamda * t, k) / Factoriel(k));
+        }
+       
+    }
+}
